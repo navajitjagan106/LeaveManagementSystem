59eb0aac6d04acdc9c9346aa5b0778dcbbc7f694
Navajit <navajitjagan@gmail.com>
2026-04-03 14:20:42 +0530

    Backend Deployment

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,8 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
