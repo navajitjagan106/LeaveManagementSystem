0e4476c65347af28cc4a683a12bb732fcc830842
Navajit <navajitjagan@gmail.com>
2026-05-05 12:11:56 +0530

    Pushed Changes-5

--- backend/src/utils/emailService.ts
+++ backend/src/utils/emailService.ts
@@ -1,7 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
